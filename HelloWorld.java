ad32987b42174f026e0d88f4e7e9937dd5732d2a
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-02-05 11:57:51 +1100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Author: Atharv Damle
+// Obligatory hello world program. Display a sentence on the screen.
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
