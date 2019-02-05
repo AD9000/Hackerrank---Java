4b9336e793d61639513006a6f803482cc16d7f92
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-02-05 12:38:00 +1100

    Create IntToString.java

---
+++ IntToString.java
@@ -0,0 +1,5 @@
+// Author: Atharv Damle
+// Code to convert int data type to a String data type in java 8
+// Full Question: https://www.hackerrank.com/challenges/java-int-to-string/problem
+
+String s = Integer.toString(n);
