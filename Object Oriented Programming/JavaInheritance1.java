4160bf0550c37306757565a547890c56932b4cbb
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-02-07 13:53:25 +1100

    Create JavaInheritance1.java

---
+++ Object Oriented Programming/JavaInheritance1.java
@@ -0,0 +1,9 @@
+// Author: Atharv Damle
+// Create a class to override the fly() method of parent Animal class
+// Full Question: https://www.hackerrank.com/challenges/java-inheritance-1/problem
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+}
