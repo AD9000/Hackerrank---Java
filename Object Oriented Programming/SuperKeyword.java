7bac0dc5ca61364236d45ee4a656b650d4b474d0
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-02-07 14:06:58 +1100

    Create SuperKeyword.java

---
+++ Object Oriented Programming/SuperKeyword.java
@@ -0,0 +1,6 @@
+// Author: Atharv Damle
+// Code to demonstrate use of super keyword.
+// Full Question: https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem
+
+// The super keyword is used to refer to the parent class. Here the super keyword calls the define_me() class of the parent class
+String temp=super.define_me();
